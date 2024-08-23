886291184e539c8cd79da8bc1e6e2719bbcf6bce
Aditya kumar Choudhary <choudhaaditya765@gmail.com>
2024-08-23 20:44:42 +0530

    Add files via upload
     This is the Razorpay web page .

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+fontFamily:
+{
+mullish:["Mulish" , "sans-serif"],
+
+
+},
+colors:
+{
+  deepBlue:"#02042a",
+  LightBlue:"#2b84e9",
+  LightBlue300:"#4b94ed",
+  LightBlue500:"#0b72e7",
+  greenLight:"#61cea6",
+  grayText: "#808597",
+  LightGray:"344a6c",
+  grayBlue:"#344a6c",
+  deepBlueHead:"#162f56",
+  gray2:"#525a76",
+
+}
+
+
+
+    },
+  },
+  plugins: [],
+}
+
